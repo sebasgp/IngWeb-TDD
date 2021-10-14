9ebbd9e8a42e4f61aee19dc13bc8ebc6b97dbc46
sebasgp <sebastian.gallegop@udea.edu.co>
2021-10-14 09:12:09 -0500

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,33 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularTesting';
+  title = 'angularTesting - Ing Web';
   numero: string = '';
 
   convertirARomano(numero) {
-    var numRomanos = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-    var nStr = numero.toString();
-    var numLen = nStr.length;
-    var romano = '';
-    for (var i = 0; i < numLen; i++) {
-      var numStr = nStr.substr(i, 1);
-      var numPos = (numLen - i - 1) * 2;
-      if (numStr > 0 && numStr <= 3) {
-        romano += this.duplicarLetras(numRomanos[numPos], numStr);
-      } else if (numStr == 4) {
-        romano += numRomanos[numPos];
-        romano += numRomanos[numPos + 1];
-      } else if (numStr >= 5 && numStr <= 8) {
-        romano += numRomanos[numPos + 1];
-        romano += this.duplicarLetras(numRomanos[numPos], numStr - 5);
-      } else if (numStr == 9) {
-        romano += numRomanos[numPos];
-        romano += numRomanos[numPos + 2];
+    if (numero < 1 || numero > 3999) {
+      this.numero = 'Ingresa un número entre 1 y 3999';
+      romano = "Ingresa un número entre 1 y 3999";
+      return romano;
+    } else {
+      var numRomanos = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+      var nStr = numero.toString();
+      var numLen = nStr.length;
+      var romano = '';
+      for (var i = 0; i < numLen; i++) {
+        var numStr = nStr.substr(i, 1);
+        var numPos = (numLen - i - 1) * 2;
+        if (numStr > 0 && numStr <= 3) {
+          romano += this.duplicarLetras(numRomanos[numPos], numStr);
+        } else if (numStr == 4) {
+          romano += numRomanos[numPos];
+          romano += numRomanos[numPos + 1];
+        } else if (numStr >= 5 && numStr <= 8) {
+          romano += numRomanos[numPos + 1];
+          romano += this.duplicarLetras(numRomanos[numPos], numStr - 5);
+        } else if (numStr == 9) {
+          romano += numRomanos[numPos];
+          romano += numRomanos[numPos + 2];
+        }
       }
+      /*     const romanot = 'Este es el numero ingresado en romano';
+       */
+      this.numero = romano;
+      return romano;
     }
-    const romanot = 'Este es el numero ingresado en romano';
-    console.log(numero + ' : ' + romano);
-    this.numero=romano;
   }
 
   duplicarLetras(a, n) {
@@ -42,4 +49,5 @@ export class AppComponent {
     }
     return romano;
   }
+
 }
